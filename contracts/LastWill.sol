eb7d72bb0be5b7e48e04953e6bc199c30dec6fae
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-08-03 19:36:10 +0300

    update dependencies, add more docs, change solhint to solium, change indentations from 4 to 2

--- contracts/LastWill.sol
+++ contracts/LastWill.sol
@@ -11,190 +11,226 @@ import "sc-library/contracts/ERC223/ERC223Basic.sol";
  * The base LastWill contract. Check method must be overridden.
  */
 contract LastWill is SoftDestruct, Checkable, ERC223Receiver {
-    struct RecipientPercent {
-        address recipient;
-        uint8 percent;
+  struct RecipientPercent {
+    address recipient;
+    uint8 percent;
+  }
+
+  /**
+   * Maximum length of token contracts addresses list
+   */
+  uint public constant TOKEN_ADDRESSES_LIMIT = 10;
+
+  /**
+   * Addresses of token contracts
+   */
+  address[] private tokenAddresses;
+
+  /**
+   * Recipient addresses and corresponding % of funds.
+   */
+  RecipientPercent[] private percents;
+
+  // ------------ EVENTS ----------------
+  // Occurs when contract was killed.
+  event Killed(bool byUser);
+  // Occurs when funds were sent.
+  event FundsAdded(address indexed from, uint amount);
+  // Occurs when accident leads to sending funds to recipient.
+  event FundsSent(address recipient, uint amount, uint percent);
+  // Occurs when accident leads to sending tokens to recipient
+  event TokensSent(address token, address recipient, uint amount, uint percent);
+
+  // ------------ CONSTRUCT -------------
+  constructor(
+    address _targetUser,
+    address[] _recipients,
+    uint[] _percents
+  )
+    public
+    SoftDestruct(_targetUser)
+  {
+    require(_recipients.length == _percents.length);
+    percents.length = _recipients.length;
+    // check percents
+    uint summaryPercent = 0;
+    for (uint i = 0; i < _recipients.length; i++) {
+      address recipient = _recipients[i];
+      uint percent = _percents[i];
+
+      require(recipient != 0x0);
+      summaryPercent += percent;
+      percents[i] = RecipientPercent(recipient, uint8(percent));
     }
+    require(summaryPercent == 100);
+  }
 
-    /**
-     * Maximum length of token contracts addresses list
-     */
-    uint public constant TOKEN_ADDRESSES_LIMIT = 10;
+  // ------------ FALLBACK -------------
+  // Must be less than 2300 gas
+  function() public payable onlyAlive() notTriggered {
+    emit FundsAdded(msg.sender, msg.value);
+  }
 
-    /**
-     * Addresses of token contracts
-     */
-    address[] private tokenAddresses;
+  /**
+   * @dev Adds token addresses.
+   *
+   * @param _contracts Token contracts list to add.
+   */
+  function addTokenAddresses(address[] _contracts) external onlyTarget notTriggered {
+    require(tokenAddresses.length + _contracts.length <= TOKEN_ADDRESSES_LIMIT);
+    for (uint i = 0; i < _contracts.length; i++) {
+      _addTokenAddress(_contracts[i]);
+    }
+  }
 
-    /**
-     * Recipient addresses and corresponding % of funds.
-     */
-    RecipientPercent[] private percents;
+  /**
+   * @dev Adds token address.
+   *
+   * @param _contract Token contract to add.
+   */
+  function addTokenAddress(address _contract) public onlyTarget notTriggered {
+    require(tokenAddresses.length < TOKEN_ADDRESSES_LIMIT);
+    _addTokenAddress(_contract);
+  }
 
-    // ------------ EVENTS ----------------
-    // Occurs when contract was killed.
-    event Killed(bool byUser);
-    // Occurs when funds were sent.
-    event FundsAdded(address indexed from, uint amount);
-    // Occurs when accident leads to sending funds to recipient.
-    event FundsSent(address recipient, uint amount, uint percent);
-    // Occurs when accident leads to sending tokens to recipient
-    event TokensSent(address token, address recipient, uint amount, uint percent);
+  /**
+   * @dev Removes token address.
+   *
+   * @param _contracts Token contract to remove.
+   */
+  function deleteTokenAddress(address _contract) public onlyTarget {
+    require(_contract != address(0));
+    require(internalIsTokenAddressAlreadyInList(_contract));
+    for (uint i = 0; i < tokenAddresses.length; i++) {
+      if (tokenAddresses[i] == _contract) {
+        tokenAddresses[i] = tokenAddresses[tokenAddresses.length - 1];
+        delete tokenAddresses[tokenAddresses.length - 1];
+        tokenAddresses.length--;
+        break;
+      }
+    }
+  }
 
-    // ------------ CONSTRUCT -------------
-    constructor(
-        address _targetUser,
-        address[] _recipients,
-        uint[] _percents
-    ) public SoftDestruct(_targetUser) {
-        require(_recipients.length == _percents.length);
-        percents.length = _recipients.length;
-        // check percents
-        uint summaryPercent = 0;
-        for (uint i = 0; i < _recipients.length; i++) {
-            address recipient = _recipients[i];
-            uint percent = _percents[i];
+  /**
+   * @dev Limit check execution only for alive contract.
+   */
+  function check() public onlyAlive payable {
+    super.check();
+  }
 
-            require(recipient != 0x0);
-            summaryPercent += percent;
-            percents[i] = RecipientPercent(recipient, uint8(percent));
+  /**
+   * @dev Extends super method to add event producing.
+   */
+  function kill() public {
+    super.kill();
+    emit Killed(true);
+  }
+
+  /**
+   * @dev Rejects not listed tokens.
+   */
+  function tokenFallback(address, uint, bytes) public {
+    require(internalIsTokenAddressAlreadyInList(msg.sender));
+  }
+
+  /**
+   * @dev Returns added token addresses.
+   */
+  function getTokenAddresses() public view returns (address[]) {
+    return tokenAddresses;
+  }
+
+  // ------------ INTERNAL -------------
+  function _addTokenAddress(address _contract) internal {
+    require(_contract != address(0));
+    require(!internalIsTokenAddressAlreadyInList(_contract));
+    tokenAddresses.push(_contract);
+  }
+
+  /**
+   * @dev Calculate amounts to transfer corresponding to the percents.
+   *
+   * @param balance
+   */
+  function internalCalculateAmounts(uint _balance) internal view returns (uint[] amounts) {
+    uint remainder = _balance;
+    amounts = new uint[](percents.length);
+    for (uint i = 0; i < percents.length; i++) {
+      if (i + 1 == percents.length) {
+        amounts[i] = remainder;
+        break;
+      }
+      uint amount = _balance * percents[i].percent / 100;
+      amounts[i] = amount;
+      remainder -= amount;
+    }
+  }
+
+  /**
+   * @dev Distribute funds between recipients in corresponding by percents.
+   */
+  function internalDistributeFunds() internal {
+    uint[] memory amounts = internalCalculateAmounts(address(this).balance);
+
+    for (uint i = 0; i < amounts.length; i++) {
+      uint amount = amounts[i];
+      address recipient = percents[i].recipient;
+      uint percent = percents[i].percent;
+
+      if (amount == 0) {
+        continue;
+      }
+
+      recipient.transfer(amount);
+      emit FundsSent(recipient, amount, percent);
+    }
+  }
+
+  /**
+   * @dev Distribute tokens between recipients in corresponding by percents.
+   */
+  function internalDistributeTokens() internal {
+    for (uint i = 0; i < tokenAddresses.length; i++) {
+      ERC20Basic token = ERC20Basic(tokenAddresses[i]);
+      uint[] memory amounts = internalCalculateAmounts(token.balanceOf(this));
+
+      for (uint j = 0; j < amounts.length; j++) {
+        uint amount = amounts[j];
+        address recipient = percents[j].recipient;
+        uint percent = percents[j].percent;
+
+        if (amount == 0) {
+          continue;
         }
-        require(summaryPercent == 100);
+
+        token.transfer(recipient, amount);
+        emit TokensSent(
+          token,
+          recipient,
+          amount,
+          percent
+        );
+      }
     }
+  }
 
-    // ------------ FALLBACK -------------
-    // Must be less than 2300 gas
-    function () public payable onlyAlive() notTriggered {
-        emit FundsAdded(msg.sender, msg.value);
+  /**
+   * @dev Do inner action if check was success.
+   */
+  function internalAction() internal {
+    internalDistributeFunds();
+    internalDistributeTokens();
+  }
+
+  /**
+   * @dev Is token contract address added.
+   */
+  function internalIsTokenAddressAlreadyInList(address _contract) internal view returns (bool) {
+    for (uint i = 0; i < tokenAddresses.length; i++) {
+      if (tokenAddresses[i] == _contract) {
+        return true;
+      }
     }
-
-    function addTokenAddresses(address[] _contracts) external onlyTarget notTriggered {
-        require(tokenAddresses.length + _contracts.length <= TOKEN_ADDRESSES_LIMIT);
-        for (uint i = 0; i < _contracts.length; i++) {
-            _addTokenAddress(_contracts[i]);
-        }
-    }
-
-    function addTokenAddress(address _contract) public onlyTarget notTriggered {
-        require(tokenAddresses.length < TOKEN_ADDRESSES_LIMIT);
-        _addTokenAddress(_contract);
-    }
-
-    function deleteTokenAddress(address _contract) public onlyTarget {
-        require(_contract != address(0));
-        require(isTokenAddressAlreadyInList(_contract));
-        for (uint i = 0; i < tokenAddresses.length; i++) {
-            if (tokenAddresses[i] == _contract) {
-                tokenAddresses[i] = tokenAddresses[tokenAddresses.length - 1];
-                delete tokenAddresses[tokenAddresses.length - 1];
-                tokenAddresses.length--;
-                break;
-            }
-        }
-    }
-
-    /**
-     * Limit check execution only for alive contract.
-     */
-    function check() public onlyAlive payable {
-        super.check();
-    }
-
-    /**
-     * Extends super method to add event producing.
-     */
-    function kill() public {
-        super.kill();
-        emit Killed(true);
-    }
-
-    /**
-     * Reject not listed tokens.
-     */
-    function tokenFallback(address, uint, bytes) public {
-        require(isTokenAddressAlreadyInList(msg.sender));
-    }
-
-    function getTokenAddresses() public view returns (address[]) {
-        return tokenAddresses;
-    }
-
-    // ------------ INTERNAL -------------
-    function _addTokenAddress(address _contract) internal {
-        require(_contract != address(0));
-        require(!isTokenAddressAlreadyInList(_contract));
-        tokenAddresses.push(_contract);
-    }
-
-    /**
-     * Calculate amounts to transfer corresponding to the percents.
-     */
-    function calculateAmounts(uint balance) internal view returns (uint[] amounts) {
-        uint remainder = balance;
-        amounts = new uint[](percents.length);
-        for (uint i = 0; i < percents.length; i++) {
-            if (i + 1 == percents.length) {
-                amounts[i] = remainder;
-                break;
-            }
-            uint amount = balance * percents[i].percent / 100;
-            amounts[i] = amount;
-            remainder -= amount;
-        }
-    }
-
-    /**
-     * Distribute funds between recipients in corresponding by percents.
-     */
-    function distributeFunds() internal {
-        uint[] memory amounts = calculateAmounts(address(this).balance);
-
-        for (uint i = 0; i < amounts.length; i++) {
-            uint amount = amounts[i];
-            address recipient = percents[i].recipient;
-            uint percent = percents[i].percent;
-
-            if (amount == 0) {
-                continue;
-            }
-
-            recipient.transfer(amount);
-            emit FundsSent(recipient, amount, percent);
-        }
-    }
-
-    function distributeTokens() internal {
-        for (uint i = 0; i < tokenAddresses.length; i++) {
-            ERC20Basic token = ERC20Basic(tokenAddresses[i]);
-            uint[] memory amounts = calculateAmounts(token.balanceOf(this));
-
-            for (uint j = 0; j < amounts.length; j++) {
-                uint amount = amounts[j];
-                address recipient = percents[j].recipient;
-                uint percent = percents[j].percent;
-
-                if (amount == 0) {
-                    continue;
-                }
-
-                token.transfer(recipient, amount);
-                emit TokensSent(token, recipient, amount, percent);
-            }
-        }
-    }
-
-    /**
-     * @dev Do inner action if check was success.
-     */
-    function internalAction() internal {
-        distributeFunds();
-        distributeTokens();
-    }
-
-    function isTokenAddressAlreadyInList(address _contract) internal view returns (bool) {
-        for (uint i = 0; i < tokenAddresses.length; i++) {
-            if (tokenAddresses[i] == _contract) return true;
-        }
-        return false;
-    }
+    return false;
+  }
 }
--- contracts/SimpleERC223Token.sol
+++ contracts/SimpleERC223Token.sol
@@ -4,7 +4,7 @@ import "sc-library/contracts/ERC223/ERC223Token.sol";
 
 
 contract SimpleERC223Token is ERC223Token {
-    constructor() public {
-        balances[msg.sender] = 1000;
-    }
+  constructor() public {
+    balances[msg.sender] = 1000;
+  }
 }
